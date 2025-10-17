4e5b65592e7de20e0ef53f39bf9b5b16626f061c
Ana Clara <anacsds1@gmail.com>
2025-10-17 20:02:04 -0300

    Iniciando Blog

---
+++ BlogeMural/blog.js
@@ -0,0 +1,35 @@
+const sistBlog = [ 
+    { 
+    "BlogMural": { 
+        "titulo": "Blog ou Mural de histórias", 
+        "descricao": "Um espaço para compartilhar depoimentos, histórias e conteúdos inspiradores.", 
+    }
+}
+]
+    const sistPostagens = [ 
+        { 
+            "idPost": "p1", "Nome": "Digite aqui...", 
+            "NovaHistória": "Digite aqui...", }, 
+        ] 
+        { "sistemaFeedbackReacoes" = [ 
+            { "id": "Amei" , 
+                "icone": "❤"
+                }, 
+                { "id": "Legal", 
+                    "icone": " " 
+
+                }, 
+                { "id": "Gostei", 
+                    "icone": " " 
+
+                }, 
+            ] 
+        } 
+                { "sistemaComentarios" = 
+                    { "nomeUsuario": "Digite aqui...", 
+                        "texto": "Digite aqui..."
+                        }
+                    } 
+
+
+                
